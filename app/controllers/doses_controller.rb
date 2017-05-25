b8703e059d84482c574242b5369a39ecc364c1e7
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-05-25 16:31:32 +0100

    created a mister cocktail app

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,38 @@
 class DosesController < ApplicationController
+before_action :set_dose, only: [:destroy]
+
+  def new
+    @dose = Dose.new
+    # @ingredient = Ingredient.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @dose.ingredient = @ingredient
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+
+  end
+
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+   def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
 end
